ff54bd1ac00b314d23d141d1ddc3bc020c5072ee
ausov <ausov@golamago.com>
2018-02-22 00:14:28 +0300

    added implementation

--- BaseAlgorithms.Tests/InverseInsertionSortTests.cs
+++ BaseAlgorithms.Tests/InverseInsertionSortTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaseAlgorithms.Tests
 {
@@ -8,7 +9,19 @@ namespace BaseAlgorithms.Tests
         [TestMethod]
         public void TestSort()
         {
+            var arr = new[] { 5, 1, 4, 2, 3 };
+            var res = InverseInsertionSort.Sort(arr);
+            Assert.IsTrue(res.SequenceEqual(new[] { 5, 4, 3, 2, 1 }));
 
+            arr = new[] { 5, 4, 3, 2, 1};
+
+            res = InverseInsertionSort.Sort(arr);
+            Assert.IsTrue(res.SequenceEqual(new[] { 5, 4, 3, 2, 1 }));
+
+            arr = new[] { 1, 2, 3, 4, 5 };
+
+            res = InverseInsertionSort.Sort(arr);
+            Assert.IsTrue(res.SequenceEqual(new[] { 5, 4, 3, 2, 1 }));
         }
     }
 }
--- BaseAlgorithms/InverseInsertionSort.cs
+++ BaseAlgorithms/InverseInsertionSort.cs
@@ -1,10 +1,63 @@
-﻿namespace BaseAlgorithms
+﻿using System;
+
+namespace BaseAlgorithms
 {
     public class InverseInsertionSort
     {
-        public static T Sort<T>(T[] arr) where T: struct
+        public static T[] Sort<T>(T[] arr) where T : struct,
+            IComparable,
+            IComparable<T>,
+            IConvertible,
+            IEquatable<T>,
+            IFormattable
         {
-            return default(T);
+
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                var j = i + 1;
+                while (j > 0)
+                {
+                    if (IsGreaterThan(arr[j], arr[j-1]))
+                    {
+                        var temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                    j--;
+                }
+            }
+
+            return arr;
         }
+
+        public static bool IsGreaterThan<T>(T actual, T comp) where T : IComparable<T>
+            => actual.CompareTo(comp) > 0;
+
+        public static T[] ForwardSort<T>(T[] arr) where T : struct,
+            IComparable,
+            IComparable<T>,
+            IConvertible,
+            IEquatable<T>,
+            IFormattable
+        {
+
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                var j = i + 1;
+                while (j > 0)
+                {
+                    if (IsGreaterThan(arr[j - 1], arr[j]))
+                    {
+                        var temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                    j--;
+                }
+            }
+
+            return arr;
+        }
+
     }
 }

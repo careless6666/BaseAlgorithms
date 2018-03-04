19a7b1ef458d7c5daecf48add93f756fc06fef72
ausov <ausov@golamago.com>
2018-03-04 20:05:57 +0300

    in place merge sort

---
+++ BaseAlgorithms.Tests/InPlaceMergeSort.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class InPlaceMergeSort
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            var res = BaseAlgorithms.InPlaceMergeSort.Sort(arr);
+            Assert.IsTrue(res.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}
---
+++ BaseAlgorithms/InPlaceMergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BaseAlgorithms
+{
+    public class InPlaceMergeSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            MergeSort(arr, 0, arr.Length - 1);
+            return arr;
+        }
+
+        private static void MergeSort(int[] arr, int left, int right)
+        {
+            if (right - left == 0)
+            {//only one element.
+                //no Swap
+            }
+            else if (right - left == 1)
+            {//only two elements and swaps them
+                if (arr[left] > arr[right])
+                    Swap(arr, left, right);
+            }
+            else
+            {
+                var middlePoint = (left + right) / 2;
+
+                MergeSort(arr, left, middlePoint);//sort the left side
+                MergeSort(arr, middlePoint + 1, right);//sort the right side
+                Merge(arr, left, right, middlePoint);//combines them
+            }
+        }
+
+        static void Merge(int[] arr, int left, int right, int mid)
+        {
+            var i = left;
+            while (i <= mid)
+            {
+                if (arr[i] > arr[mid + 1])
+                {
+                    Swap(arr, i, mid + 1);
+                    Push(arr, mid + 1, right);
+                }
+                i++;
+            }
+        }
+
+        static void Swap(int[] arr, int left, int right)
+        {
+            arr[left] = arr[left] ^ arr[right];
+            arr[right] = arr[left] ^ arr[right];
+            arr[left] = arr[left] ^ arr[right];
+        }
+
+        static void Push(int[] arr, int s, int e)
+        {
+            for (var i = s; i < e; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                    Swap(arr, i, i + 1);
+            }
+        }
+    }
+}

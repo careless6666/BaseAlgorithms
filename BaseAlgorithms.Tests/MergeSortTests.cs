887fe9aa7477975e5822956230240c258061c5ff
ausov <ausov@golamago.com>
2018-03-04 17:35:17 +0300

    added simple implementation merge sort

---
+++ BaseAlgorithms.Tests/MergeSortTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+        [TestMethod]
+        public void TestSorts()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            var res = MergeSort.Sort(arr);
+            Assert.IsTrue(res.SequenceEqual(new []{3, 9, 26, 38, 41, 49, 52, 57}));
+        }
+    }
+}
--- BaseAlgorithms/MergeSort.cs
+++ BaseAlgorithms/MergeSort.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BaseAlgorithms
 {
     public class MergeSort
     {
+        public static int[] Sort(int[] arr)
+        {
+            if (arr.Length == 1)
+                return arr;
+
+            var middlePoint = arr.Length / 2;
+            return Merge(Sort(arr.Take(middlePoint).ToArray()), 
+                Sort(arr.Skip(middlePoint).ToArray()));
+        }
+
+        private static int[] Merge(int[] leftArr, int[] rightArr)
+        {
+            var leftCounter = 0;
+            var rightCounter = 0;
+
+            var merged = new int[leftArr.Length + rightArr.Length];
+            for (var i = 0; i < leftArr.Length + rightArr.Length; i++)
+            {
+                if (rightCounter < rightArr.Length && leftCounter < leftArr.Length)
+                {
+                    if (leftArr[leftCounter] > rightArr[rightCounter])
+                        merged[i] = rightArr[rightCounter++];
+                    else
+                        merged[i] = leftArr[leftCounter++];
+                }
+                else
+                {
+                    if (rightCounter < rightArr.Length)
+                        merged[i] = rightArr[rightCounter++];
+                    else
+                        merged[i] = leftArr[leftCounter++];
+                }
+            }
+
+            return merged;
+        }
     }
 }

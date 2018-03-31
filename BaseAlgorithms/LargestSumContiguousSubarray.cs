53130f4fc2ec3e998fab6c9716cdd1ec99467861
ausov <ausov@golamago.com>
2018-03-31 14:10:27 +0300

    refact code

--- BaseAlgorithms/LargestSumContiguousSubarray.cs
+++ BaseAlgorithms/LargestSumContiguousSubarray.cs
@@ -4,22 +4,22 @@
     {
         public static int MaxSubArraySum(int[] a)
         {
-            int size = a.Length;
-            int max_so_far = int.MinValue,
-                max_ending_here = 0;
+            var size = a.Length;
+            int maxSoFar = int.MinValue,
+                maxEndingHere = 0;
 
-            for (int i = 0; i < size; i++)
+            for (var i = 0; i < size; i++)
             {
-                max_ending_here = max_ending_here + a[i];
+                maxEndingHere = maxEndingHere + a[i];
 
-                if (max_so_far < max_ending_here)
-                    max_so_far = max_ending_here;
+                if (maxSoFar < maxEndingHere)
+                    maxSoFar = maxEndingHere;
 
-                if (max_ending_here < 0)
-                    max_ending_here = 0;
+                if (maxEndingHere < 0)
+                    maxEndingHere = 0;
             }
 
-            return max_so_far;
+            return maxSoFar;
         }
     }
 }

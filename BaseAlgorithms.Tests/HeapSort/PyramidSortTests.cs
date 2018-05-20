84ae11b4eeedd2eccf8f1637fa6720d0b841e1bd
ausov <ausov@golamago.com>
2018-05-20 17:33:14 +0300

    added pyramid sorting

---
+++ BaseAlgorithms.Tests/HeapSort/PyramidSortTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using BaseAlgorithms.HeapSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.HeapSort
+{
+    [TestClass]
+    public class PyramidSortTests
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            PyramidSort.Sort(arr, arr.Length);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+
+    }
+}
---
+++ BaseAlgorithms/HeapSort/PyramidSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseAlgorithms.HeapSort
+{
+    public class PyramidSort
+    {
+        static int Add2Pyramid(IList<int> arr, int i, int n)
+        {
+            int imax;
+            if ((2 * i + 2) < n)
+            {
+                if (arr[2 * i + 1] < arr[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else
+                imax = 2 * i + 1;
+
+            if (imax >= n)
+                return i;
+
+            if (arr[i] >= arr[imax])
+                return i;
+
+            var buf = arr[i];
+            arr[i] = arr[imax];
+            arr[imax] = buf;
+
+            if (imax < n / 2)
+                i = imax;
+
+            return i;
+        }
+
+        public static void Sort(int[] arr, int len)
+        {
+            //step 1: building the pyramid
+            for (int i = len / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = Add2Pyramid(arr, i, len);
+                if (prev_i != i) ++i;
+            }
+
+            //step 2: sorting
+            for (int k = len - 1; k > 0; --k)
+            {
+                var buf = arr[0];
+                arr[0] = arr[k];
+                arr[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = Add2Pyramid(arr, i, k);
+                }
+            }
+        }
+    }
+}

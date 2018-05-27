f4986dee29d9aceccb08d7fb354c02a5e380adca
ausov <ausov@golamago.com>
2018-05-27 17:23:41 +0300

    added priority queue

---
+++ BaseAlgorithms.Tests/HeapSort/PriorityQueueTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BaseAlgorithms.HeapSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.HeapSort
+{
+    [TestClass]
+    public class PriorityQueueTests
+    {
+        [TestMethod]
+        public void TestPriorityQeue()
+        {
+            var e1 = new Employee("Aiden", 1.0);
+            var e2 = new Employee("Baker", 2.0);
+            var e3 = new Employee("Chung", 3.0);
+            var e4 = new Employee("Dunne", 4.0);
+            var e5 = new Employee("Eason", 5.0);
+            var e6 = new Employee("Flynn", 6.0);
+            var e7 = new Employee("ololo", 2.5);
+
+            var pq = new PriorityQueue<Employee>();
+
+            pq.Enqueue(e5);
+            pq.Enqueue(e3);
+            pq.Enqueue(e6);
+            pq.Enqueue(e4);
+            pq.Enqueue(e1);
+            pq.Enqueue(e2);
+            pq.Enqueue(e7);
+
+            var res = pq.Dequeue();
+            Assert.AreEqual(1, res.priority);
+
+            res = pq.Dequeue();
+            Assert.AreEqual(2, res.priority);
+
+            res = pq.Dequeue();
+            Assert.AreEqual(2.5, res.priority);
+
+        }
+    }
+}
---
+++ BaseAlgorithms/HeapSort/PriorityQueue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseAlgorithms.HeapSort
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        private readonly List<T> _data;
+
+        public PriorityQueue()
+        {
+            _data = new List<T>();
+        }
+
+        public bool IsConsistent()
+        {
+            if (_data.Count == 0) return true;
+            var li = _data.Count - 1; // last index
+            for (var pi = 0; pi < _data.Count; ++pi) // each parent index
+            {
+                var lci = 2 * pi + 1; // left child index
+                var rci = 2 * pi + 2; // right child index
+                if (lci <= li && _data[pi].CompareTo(_data[lci]) > 0) return false;
+                if (rci <= li && _data[pi].CompareTo(_data[rci]) > 0) return false;
+            }
+            return true; // Passed all checks
+        }
+
+        public void Enqueue(T item)
+        {
+            _data.Add(item);
+            var ci = _data.Count - 1;
+            while (ci > 0)
+            {
+                var pi = (ci - 1) / 2;
+                if (_data[ci].CompareTo(_data[pi]) >= 0)
+                    break;
+                var tmp = _data[ci]; _data[ci] = _data[pi]; _data[pi] = tmp;
+                ci = pi;
+            }
+        }
+
+        public T Dequeue()
+        {
+            // Assumes pq isn't empty
+            var li = _data.Count - 1;
+            var frontItem = _data[0];
+            _data[0] = _data[li];
+            _data.RemoveAt(li);
+
+            --li;
+            var pi = 0;
+            while (true)
+            {
+                var ci = pi * 2 + 1;
+                if (ci > li) break;
+                var rc = ci + 1;
+                if (rc <= li && _data[rc].CompareTo(_data[ci]) < 0)
+                    ci = rc;
+                if (_data[pi].CompareTo(_data[ci]) <= 0) break;
+                var tmp = _data[pi]; _data[pi] = _data[ci]; _data[ci] = tmp;
+                pi = ci;
+            }
+            return frontItem;
+        }
+
+        public T Peek()
+        {
+            var frontItem = _data[0];
+            return frontItem;
+        }
+    }
+    
+    public class Employee : IComparable<Employee>
+    {
+        public string lastName;
+        public double priority; // smaller values are higher priority
+
+        public Employee(string lastName, double priority)
+        {
+            this.lastName = lastName;
+            this.priority = priority;
+        }
+
+        public override string ToString()
+        {
+            return "(" + lastName + ", " + priority.ToString("F1") + ")";
+        }
+
+        public int CompareTo(Employee other)
+        {
+            if (priority < other.priority) return -1;
+            if (priority > other.priority) return 1;
+            return 0;
+        }
+    } // Employee
+
+}

295d7430d6737d68462006bc5e033d58f64d5885
ausov <ausov@golamago.com>
2018-06-02 18:05:22 +0300

    added quick sort

---
+++ BaseAlgorithms.Tests/QuickSortTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class QuickSortTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            QuickSort.Sort(arr, 0, arr.Length-1);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}
---
+++ BaseAlgorithms/Quicksort.cs
@@ -0,0 +1,46 @@
+﻿namespace BaseAlgorithms
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Sort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Sort(elements, i, right);
+            }
+        }
+    }
+}

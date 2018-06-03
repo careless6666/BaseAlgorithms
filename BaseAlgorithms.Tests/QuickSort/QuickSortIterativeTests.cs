6a2a24a9e74c8757bcaa98a38f8e3cd1ebb7b281
ausov <ausov@golamago.com>
2018-06-03 19:43:01 +0300

    added iterative quick sort

---
+++ BaseAlgorithms.Tests/QuickSort/QuickSortIterativeTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.QuickSort
+{
+    [TestClass]
+    public class QuickSortIterativeTests
+    {
+        [TestMethod]
+        public void Sort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            QuickSortIterative.Sort(ref arr);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}
---
+++ BaseAlgorithms/QuickSort/QuickSortIterative.cs
@@ -0,0 +1,63 @@
+﻿namespace BaseAlgorithms
+{
+    public class QuickSortIterative
+    {
+        public static void Sort(ref int[] data)
+        {
+            var startIndex = 0;
+            var endIndex = data.Length - 1;
+            var top = -1;
+            var stack = new int[data.Length];
+
+            stack[++top] = startIndex;
+            stack[++top] = endIndex;
+
+            while (top >= 0)
+            {
+                endIndex = stack[top--];
+                startIndex = stack[top--];
+
+                var p = Partition(ref data, startIndex, endIndex);
+
+                if (p - 1 > startIndex)
+                {
+                    stack[++top] = startIndex;
+                    stack[++top] = p - 1;
+                }
+
+                if (p + 1 >= endIndex)
+                    continue;
+
+                stack[++top] = p + 1;
+                stack[++top] = endIndex;
+            }
+        }
+
+        private static int Partition(ref int[] data, int left, int right)
+        {
+            var x = data[right];
+            var i = (left - 1);
+
+            for (var j = left; j <= right - 1; ++j)
+            {
+                if (data[j] <= x)
+                {
+                    ++i;
+                    Swap(ref data[i], ref data[j]);
+                }
+            }
+
+            Swap(ref data[i + 1], ref data[right]);
+
+            return (i + 1);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}

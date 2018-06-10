8a489a642d245f313fed0bfb2597afa79e9ac442
ausov <ausov@golamago.com>
2018-06-10 13:45:27 +0300

    added counting sorting

---
+++ BaseAlgorithms.Tests/CountingSortTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class CountingSortTests
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            CountingSort.Sort(arr);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}
---
+++ BaseAlgorithms/CountingSort.cs
@@ -0,0 +1,45 @@
+﻿namespace BaseAlgorithms
+{
+    public class CountingSort
+    {
+        public static void Sort(int[] arr)
+        {
+            var n = arr.Length;
+
+            // The output character array that
+            // will have sorted arr
+            var output = new int[n];
+
+            // Create a count array to store 
+            // count of inidividul characters 
+            // and initialize count array as 0
+            var count = new int[256];
+
+            for (var i = 0; i < 256; ++i)
+                count[i] = 0;
+
+            // store count of each character
+            for (var i = 0; i < n; ++i)
+                ++count[arr[i]];
+
+            // Change count[i] so that count[i] 
+            // now contains actual position of 
+            // this character in output array
+            for (var i = 1; i <= 255; ++i)
+                count[i] += count[i - 1];
+
+            // Build the output character array
+            for (var i = 0; i < n; ++i)
+            {
+                output[count[arr[i]] - 1] = arr[i];
+                --count[arr[i]];
+            }
+
+            // Copy the output array to arr, so
+            // that arr now contains sorted 
+            // characters
+            for (var i = 0; i < n; ++i)
+                arr[i] = output[i];
+        }
+    }
+}

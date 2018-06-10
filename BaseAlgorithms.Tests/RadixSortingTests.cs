eb034db2cf63d7a3665f814aacd8e5f0c2646315
ausov <ausov@golamago.com>
2018-06-10 14:45:09 +0300

    added radix sorting

---
+++ BaseAlgorithms.Tests/RadixSortingTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class RadixSortingTests
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            RadixSorting.Sort(arr, arr.Length);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}

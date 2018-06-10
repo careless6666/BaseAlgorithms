6e4e619f5933a81dc52fd8fb47886e61f76277a7
ausov <ausov@golamago.com>
2018-06-10 16:34:30 +0300

    added bucket sort

---
+++ BaseAlgorithms.Tests/BucketSortTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests
+{
+    [TestClass]
+    public class BucketSortTests
+    {
+        [TestMethod]
+        public void TestSort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            BucketSort.Sort(arr);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+}
---
+++ BaseAlgorithms/BucketSort.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BaseAlgorithms
+{
+    public class BucketSort
+    {
+        public static void Sort(int[] arr) => Sort(arr, arr.Length);
+        public static void Sort(int[] arr, int n)
+        {
+            // 1) Create n empty buckets
+            var b = new List<int>[n * 100];
+            for (var i = 0; i < b.Length; i++)
+                b[i] = new List<int>();
+            
+            // 2) Put array elements in different buckets
+            for (var i = 0; i < n; i++)
+            {
+                var bi = n * arr[i]; // Index in bucket
+                b[bi].Add(arr[i]);
+            }
+
+            // 3) Sort individual buckets
+            for (var i = 0; i < n; i++)
+                b[i].Sort();
+
+            // 4) Concatenate all buckets into arr[]
+            var index = 0;
+            for (var i = 0; i < n*100; i++)
+            for (var j = 0; j < b[i].Count; j++)
+                arr[index++] = b[i][j];
+        }
+    }
+}

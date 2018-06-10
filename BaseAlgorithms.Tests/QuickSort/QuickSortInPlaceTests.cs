066dfd12c48dfd7aceede74aba9f6e6682552c7b
ausov <ausov@golamago.com>
2018-06-10 14:56:01 +0300

    added quick sort in place

---
+++ BaseAlgorithms.Tests/QuickSort/QuickSortInPlaceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.QuickSort
+{
+    [TestClass]
+    public class QuickSortInPlaceTests
+    {
+        [TestMethod]
+        public void Sort()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+
+            QuickSortInPlace.Sort(arr);
+            Assert.IsTrue(arr.SequenceEqual(new[] { 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+    }
+
+}
---
+++ BaseAlgorithms/QuickSort/QuickSortInPlace.cs
@@ -0,0 +1,42 @@
+﻿namespace BaseAlgorithms
+{
+    public class QuickSortInPlace
+    {
+        public static void Sort(int[] a) => SortInPlace(a, 0, a.Length - 1);
+            
+        public static void SortInPlace(int[] a, int first, int last)
+        {
+            if ((last - first) >= 1)
+            {
+                var pivotposition = Partition(a, first, last);
+                SortInPlace(a, first, pivotposition - 1);
+                SortInPlace(a, pivotposition + 1, last);
+            }
+
+        }
+
+        private static int Partition(int[] a, int first, int last)
+        {
+            var pivot = a[last];
+            var wallindex = first;
+            for (var currentindex = first; currentindex < last; ++currentindex)
+            {
+                if (a[currentindex] >= pivot)
+                    continue;
+
+                Swap(a, wallindex, currentindex);
+                ++wallindex;
+            }
+            Swap(a, wallindex, last);
+            return wallindex;
+
+        }
+        
+        private static void Swap(int[] a, int indexA, int indexB)
+        {
+            var temp = a[indexA];
+            a[indexA] = a[indexB];
+            a[indexB] = temp;
+        }
+    }
+}

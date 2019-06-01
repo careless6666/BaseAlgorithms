1180339a40d165b7f14ee9f8cb413c4ae7e460c1
alusov <alusov@ozon.com>
2019-06-01 10:35:10 +0300

    added bloom filter

---
+++ BaseAlgorithms.Tests/PopularTasks/BloomFilterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BaseAlgorithms.PopularTasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.PopularTasks
+{
+    [TestClass]
+    public class BloomFilterTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var myBloom = new Bloom();
+            var data = "Hello Bloom Filter";
+            myBloom.AddData(data);
+
+            var data2 = "Hello Bloom Filter";
+            Assert.AreEqual(true, myBloom.LookUp(data2));
+
+            myBloom = new Bloom();
+
+            myBloom.AddData("apple");
+            myBloom.AddData("orange");
+            myBloom.AddData("banana");
+
+            Assert.AreEqual(true, myBloom.LookUp("banana"));
+        }
+    }
+}
---
+++ BaseAlgorithms/PopularTasks/BloomFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace BaseAlgorithms.PopularTasks
+{
+    public class Bloom
+    {
+        const int m = 1000;
+        const int k = 5;
+        BitArray _bloom = new BitArray(m);
+
+        private static int[] Hashk(string s, int k)
+        {
+            var hashes = new int[k];
+            hashes[0] = Math.Abs(s.GetHashCode());
+            var r = new Random(hashes[0]);
+            for (var i = 1; i < k; i++)
+                hashes[i] = r.Next();
+            
+            return hashes;
+        }
+
+        public void AddData(string s)
+        {
+            var hashes = Hashk(s, k);
+            for (var i = 0; i < k; i++)
+                _bloom.Set(hashes[i] % m, true);
+        }
+
+        public Boolean LookUp(string s)
+        {
+            var hashes = Hashk(s, k);
+            for (var i = 0; i < k; i++)
+                if (_bloom[hashes[i] % m] == false) return false;
+            
+            return true;
+        }
+    }
+}

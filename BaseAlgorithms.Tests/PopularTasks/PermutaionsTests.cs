12e4c8434324617982dc875f4ddad8b74f8ee190
alusov <alusov@ozon.com>
2019-06-01 20:18:20 +0300

    added recursive generation

---
+++ BaseAlgorithms.Tests/PopularTasks/PermutaionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BaseAlgorithms.PopularTasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseAlgorithms.Tests.PopularTasks
+{
+    [TestClass]
+    public class PermutaionsTests
+    {
+        [TestMethod]
+        public void TestUnique()
+        {
+            var mutations = new Permutaions();
+            var res = mutations.Generate(4);
+            Assert.IsTrue(res.First().SequenceEqual(new []{ 1, 2, 3, 4}));
+            Assert.IsTrue(res.Last().SequenceEqual(new []{ 4, 3, 2, 1}));
+        }
+
+        [TestMethod]
+        public void TestRecursive()
+        {
+            var mutations = new Permutaions();
+            var res = mutations.GenerateRecursive("ABC");
+            Assert.IsTrue(res.First() == "ABC");
+            Assert.IsTrue(res.Last() == "CAB");
+        }
+
+    }
+}
---
+++ BaseAlgorithms/PopularTasks/Permutaions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaseAlgorithms.PopularTasks
+{
+    public class Permutaions
+    {
+        private List<string> ReqursiveGenerationResult;
+
+        public List<List<int>> Generate(int n)
+        {
+            var arr = new int[n];
+            for (var i = 0; i < n; i++)
+                arr[i] = i + 1;
+
+            var res = new List<List<int>> {arr.ToList()};
+
+            while (NextSet(arr, n))
+            {
+                res.Add(arr.ToList());
+            }
+
+            return res;
+        }
+
+        bool NextSet(int[] a, int n)
+        {
+            var j = n - 2;
+            while (j != -1 && a[j] >= a[j + 1]) j--;
+            if (j == -1)
+                return false; // больше перестановок нет
+            var k = n - 1;
+            while (a[j] >= a[k]) k--;
+            Swap(a, j, k);
+            int l = j + 1, r = n - 1; // сортируем оставшуюся часть последовательности
+            while (l < r)
+                Swap(a, l++, r--);
+            return true;
+        }
+
+        void Swap(int[] a, int i, int j)
+        {
+            var s = a[i];
+            a[i] = a[j];
+            a[j] = s;
+        }
+
+        public List<string> GenerateRecursive(string str)
+        {
+            ReqursiveGenerationResult = new List<string>();
+
+            var n = str.Length;
+            Permute(str, 0, n - 1);
+            return ReqursiveGenerationResult;
+        }
+
+        public void Permute(string str,
+            int l, int r)
+        {
+            if (l == r)
+                ReqursiveGenerationResult.Add(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Permute(str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+
+        public static string Swap(string a,
+            int i, int j)
+        {
+            var charArray = a.ToCharArray();
+            var temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            var s = new string(charArray);
+            return s;
+        }
+    }
+}

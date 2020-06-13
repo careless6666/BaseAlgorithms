f0c6868e6e807420266450bcb99ecb64a3c477a8
alex <alusov@ozon.ru>
2020-06-13 19:01:23 +0300

    full parallel merge sort

--- BaseAlgorithms.Tests/Sorting/MergeSort/ParallelMergeSortTests.cs
+++ BaseAlgorithms.Tests/Sorting/MergeSort/ParallelMergeSortTests.cs
@@ -13,7 +13,67 @@ namespace BaseAlgorithms.Tests.MergeSort
             
             var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
             sortEngine.Sort(arr);
-            Assert.IsTrue(arr.SequenceEqual(new []{3, 9, 26, 38, 41, 49, 52, 57}));
+            Assert.IsTrue(arr.SequenceEqual(new []{ 3, 9, 26, 38, 41, 49, 52, 57 }));
+        }
+
+        [TestMethod]
+        public void TestMergeBlocks()
+        {
+            var arr = new[] { 1, 5, 6, 7, 2, 3, 4, 8 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 0, 3, 4, 7);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 1,2,3,4,5,6,7,8 }));
+        }
+        
+        [TestMethod]
+        public void TestMergeBlocks2()
+        {
+            var arr = new[] { 1, 2, 5, 6, 3, 4, 7, 8 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 0, 3, 4, 7);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 1,2,3,4,5,6,7,8 }));
+        }
+        
+        [TestMethod]
+        public void TestMergeBlocks3()
+        {
+            var arr = new[] { 3, 41, 52, 26, 38, 4, 7, 8 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 0, 0, 1, 1);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 3, 41, 0, 0, 0, 0, 0, 0 }));
+        }
+        
+        [TestMethod]
+        public void TestMergeBlocks4()
+        {
+            var arr = new[] { 1, 3, 2, 0, 0, 0, 0, 0 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 0, 1, 2, 2);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 1, 2, 3, 0, 0, 0, 0, 0 }));
+        }
+        
+        [TestMethod]
+        public void TestMergeBlocks5()
+        {
+            var arr = new[] { 1, 2, 3, 0, 0, 0, 0, 0 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 0, 1, 2, 2);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 1, 2, 3, 0, 0, 0, 0, 0 }));
+        }
+        
+        [TestMethod]
+        public void TestMergeBlocks6()
+        {
+            var arr = new[] { 0, 0, 1, 2, 3, 0, 0, 0 };
+            var sortEngine = new BaseAlgorithms.MergeSort.ParallelMergeSort();
+            var dstArr = new int [8];
+            sortEngine.MergeTwoBlocks(arr, dstArr, 2, 3, 4, 4);
+            Assert.IsTrue(dstArr.SequenceEqual(new [] { 0, 0, 1, 2, 3, 0, 0, 0 }));
         }
     }
 }
--- BaseAlgorithms/Sorting/MergeSort/ParallelMergeSort.cs
+++ BaseAlgorithms/Sorting/MergeSort/ParallelMergeSort.cs
@@ -44,11 +44,15 @@ namespace BaseAlgorithms.MergeSort
             {
                 // Split sorting into halves
                 var middle = beginBlock + ((endBlock - beginBlock) / 2); // avoid overflows
-                if (workParallel)
+                if (false) // if (workParallel)
                 {
                     Parallel.Invoke(
-                        () => SortBlock(list, tempList, beginBlock, middle, recursionDepth + 1),
-                        () => SortBlock(list, tempList, middle + 1, endBlock, recursionDepth + 1));
+                        () =>
+                            SortBlock(list, tempList, beginBlock, middle, recursionDepth + 1)
+                        ,
+                        () =>
+                            SortBlock(list, tempList, middle + 1, endBlock, recursionDepth + 1)
+                    );
                 }
                 else
                 {
@@ -63,30 +67,125 @@ namespace BaseAlgorithms.MergeSort
                     MergeTwoBlocks(tempList, list, beginBlock, middle, middle + 1, endBlock);
             }
         }
-        
-        void MergeTwoBlocks(int[] sourceList, int[] targetList, int beginBlock1, int endBlock1, int beginBlock2, int endBlock2)
+
+        public void MergeTwoBlocks(int[] sourceList, int[] targetList, int beginBlock1, int endBlock1, int beginBlock2,
+            int endBlock2)
         {
-            for (int targetIndex = beginBlock1; targetIndex <= endBlock2; targetIndex++)
+            var median = (endBlock2 - beginBlock2) / 2 + beginBlock2;
+            var medianItem = sourceList[median];
+
+            var indexInFirstArray = InsertionSortBinarySearch(sourceList, beginBlock1, endBlock1, medianItem);
+            var sourceBeginBlock1 = beginBlock1;
+            var sourceBeginBlock2 = beginBlock2;
+
+            Parallel.Invoke(
+                () =>
+                {
+                    var i = beginBlock1;
+                    //merge elements less median
+                    while (beginBlock1 < indexInFirstArray || beginBlock2 < median)
+                    {
+                        if (beginBlock1 > indexInFirstArray)
+                        {
+                            targetList[i] = sourceList[beginBlock2++];
+                        }
+                        else if (beginBlock2 > median)
+                        {
+                            targetList[i] = sourceList[beginBlock1++];
+                        }
+                        else
+                        {
+                            if (sourceList[beginBlock1] <= sourceList[beginBlock2])
+                                targetList[i] = sourceList[beginBlock1++];
+                            else
+                                targetList[i] = sourceList[beginBlock2++];
+                        }
+
+                        i++;
+                    }
+                },
+                () =>
+                {
+                    //merge elements grather than or equals median element
+
+                    var start = indexInFirstArray;
+                    var start2 = median;
+                    var j = indexInFirstArray + (median - sourceBeginBlock2);
+
+
+                    while (start <= endBlock1 || start2 <= endBlock2)
+                    {
+                        if (start > endBlock1)
+                        {
+                            targetList[j] = sourceList[start2++];
+                        }
+                        else if (endBlock2 < start2)
+                        {
+                            targetList[j] = sourceList[start++];
+                        }
+                        else
+                        {
+                            if (sourceList[start] <= sourceList[start2])
+                                targetList[j] = sourceList[start++];
+                            else
+                                targetList[j] = sourceList[start2++];
+                        }
+
+                        j++;
+                    }
+                });
+
+            // for (int targetIndex = beginBlock1; targetIndex <= endBlock2; targetIndex++)
+            // {
+            //     if (beginBlock1 > endBlock1)
+            //     {
+            //         // Nothing is left from block1, take next element from block2
+            //         targetList[targetIndex] = sourceList[beginBlock2++];
+            //     }
+            //     else if (beginBlock2 > endBlock2)
+            //     {
+            //         // Nothing is left from block2, take next element from block1
+            //         targetList[targetIndex] = sourceList[beginBlock1++];
+            //     }
+            //     else
+            //     {
+            //         // Compare the next elements from both blocks and take the smaller one
+            //         if (sourceList[beginBlock1] - sourceList[beginBlock2] <= 0)
+            //             targetList[targetIndex] = sourceList[beginBlock1++];
+            //         else
+            //             targetList[targetIndex] = sourceList[beginBlock2++];
+            //     }
+            // }
+        }
+
+        int InsertionSortBinarySearch(int[] list, int beginBlock, int endBlock, int elementToInsert)
+        {
+            while (beginBlock <= endBlock)
             {
-                if (beginBlock1 > endBlock1)
+                int middle = beginBlock + ((endBlock - beginBlock) / 2); // avoid overflows
+
+                int comparisonRes = elementToInsert - list[middle];
+                if (comparisonRes < 0)
                 {
-                    // Nothing is left from block1, take next element from block2
-                    targetList[targetIndex] = sourceList[beginBlock2++];
+                    // elementToInsert was smaller, go to the left half
+                    endBlock = middle - 1;
                 }
-                else if (beginBlock2 > endBlock2)
+                else if (comparisonRes > 0)
                 {
-                    // Nothing is left from block2, take next element from block1
-                    targetList[targetIndex] = sourceList[beginBlock1++];
+                    // elementToInsert was bigger, go to the right half
+                    beginBlock = middle + 1;
                 }
                 else
                 {
-                    // Compare the next elements from both blocks and take the smaller one
-                    if (sourceList[beginBlock1] - sourceList[beginBlock2] <= 0)
-                        targetList[targetIndex] = sourceList[beginBlock1++];
-                    else
-                        targetList[targetIndex] = sourceList[beginBlock2++];
+                    // elementToInsert was equal, move to the right as long as elements
+                    // are equal, to get the sorting stable
+                    beginBlock = middle + 1;
+                    while ((beginBlock < endBlock) && (elementToInsert - (list[beginBlock + 1]) == 0))
+                        beginBlock++;
                 }
             }
+
+            return beginBlock;
         }
     }
 }

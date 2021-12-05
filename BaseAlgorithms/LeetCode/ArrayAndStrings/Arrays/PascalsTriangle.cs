08490455aa5ca5c1795c67fee24511bdddd5a747
alex <alusov@ozon.ru>
2021-12-05 19:25:25 +0300

    wip

--- BaseAlgorithms/LeetCode/ArrayAndStrings/Arrays/PascalsTriangle.cs
+++ BaseAlgorithms/LeetCode/ArrayAndStrings/Arrays/PascalsTriangle.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BaseAlgorithms.LeetCode.ArrayAndStrings.Arrays
 {
@@ -10,10 +11,8 @@ namespace BaseAlgorithms.LeetCode.ArrayAndStrings.Arrays
             res[0] = new[] {1};
 
             if (numRows == 1)
-            {
                 return res;
-            }
-
+            
             for (var i = 1; i < numRows; i++)
             {
                 res[i] = new int [i + 1];
@@ -26,6 +25,23 @@ namespace BaseAlgorithms.LeetCode.ArrayAndStrings.Arrays
             }
 
             return res;
+        }
+
+        public IList<int> GetRow(int rowIndex) {
+            var row = new int[rowIndex + 1];
+            row[0] = 1;
+            
+            for (var i = 0; i < rowIndex + 1; i++)
+            {
+                var lastSize = 1;
+                for (var j = i; j > 0; j--) {
+                    row[j] = row[j] + row[j - 1];
+                    lastSize = j;
+                }
+                row[lastSize - 1] = 1;
+            }
+
+            return row.ToList();
         }
     }
 }
